6af4cccc90c50b1d5f90935499814d88bad1cd3c
Yaraysa Arias Rodriguez <yaarias@cosola.org>
2017-06-06 11:15:18 -0300

    Hoteles
    Poniendo en practica aprendisajes de JavaScrit.

---
+++ hoteles.js
@@ -0,0 +1,14 @@
+ var today = new Date();
+ var hourNow = today.getHours();
+ var greeting;
+ 
+	if(hourNow>18)
+		{greeting = "Buenas Noches!";}
+		else if(hourNow>12)
+			{greeting = "Buenas Tardes!";}
+		else if(hourNow>0)	
+			{greeting = "Buenos Dias!";}
+		else 
+			{greeting = "Bienvenido!";}
+			
+	document.write('<h2>'+greeting+'</h2>');
